1e8d8915d4ff59c700d0c07d40170dd1e5dec767
IBSU Lecturer <IBSU_Lecturer@ibsu.edu.ge>
2025-05-31 15:02:56 +0400

    add-auth

--- routes/students.js
+++ routes/students.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const Studentservice = require('../services/student.service');
+const ApiSecurity = require('../middleware/apiSecurity');
 
-router.get('/all', Studentservice.getAll);
-router.post('/', Studentservice.add);
-router.get('/:id', Studentservice.getOne);
-router.delete('/:id', Studentservice.delete);
-router.put('/:id', Studentservice.update);
+router.get('/all', ApiSecurity.requireLogin, Studentservice.getAll);
+router.post('/', ApiSecurity.requirePermits('user.add'), Studentservice.add);
+router.get('/:id', ApiSecurity.requireLogin, Studentservice.getOne);
+router.delete('/:id', ApiSecurity.requirePermits('user.add'), Studentservice.delete);
+router.put('/:id', ApiSecurity.requirePermits('user.add'), Studentservice.update);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const UserService = require('../services/user.service');
+
+router.post('/register', UserService.register);
+router.post('/login', UserService.login);
 
 module.exports = router;

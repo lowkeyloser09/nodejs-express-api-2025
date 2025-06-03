65679270f885cc658cce26e69429bad2215617d2
IBSU Lecturer <IBSU_Lecturer@ibsu.edu.ge>
2025-06-03 10:29:56 +0400

    add-file-upload-download

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/test');
 var studentRouter = require('./routes/students');
+var fileRouter = require('./routes/file');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
 app.use('/students', studentRouter);
+app.use('/file', fileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
